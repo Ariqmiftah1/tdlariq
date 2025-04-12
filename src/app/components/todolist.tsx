68a87aafd5eb285bcdef4259c7eba97729f5754c
Ariqmiftah1 <ariqmiftah1@gmail.com>
2025-04-12 22:53:00 +0700

    tesss

--- src/app/components/todolist.tsx
+++ src/app/components/todolist.tsx
@@ -12,8 +12,6 @@ import {
 } from 'firebase/firestore';
 import { db } from '../lib/firebase';
 
-<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
-
 type Task = {
   id: string;
   text: string;
